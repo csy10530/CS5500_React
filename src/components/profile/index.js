bf1bb90d1a5be3427fb6636cd7e65a7766b93223
csy10530 <chen.sy909@gmail.com>
2022-04-02 22:55:57 -0400

    fixed a bug

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -86,12 +86,12 @@ const Profile = () => {
             </li>
             <li className="nav-item">
               <Link to="/profile/likes"
-                    className={`nav-link ${location.pathname.indexOf('mylikes') >= 0 ? 'active':''}`}>
+                    className={`nav-link ${location.pathname.indexOf('likes') >= 0 ? 'active':''}`}>
                 Likes</Link>
             </li>
             <li className="nav-item">
               <Link to="/profile/dislikes"
-                    className={`nav-link ${location.pathname.indexOf('mydislikes') >= 0 ? 'active':''}`}>
+                    className={`nav-link ${location.pathname.indexOf('dislikes') >= 0 ? 'active':''}`}>
                 Dislikes</Link>
             </li>
           </ul>
