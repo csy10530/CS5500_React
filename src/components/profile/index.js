20c92fdb91dd2b663b425a22c0d46b5aee8886a1
csy10530 <chen.sy909@gmail.com>
2022-04-02 22:50:16 -0400

    fixed a bug

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -101,8 +101,8 @@ const Profile = () => {
           <Route path="/mytuits" element={<MyTuits/>}/>
           <Route path="/tuits-and-replies" element={<TuitsAndReplies/>}/>
             <Route path="/media" element={<Media/>}/>
-            <Route path="/mylikes" element={<MyLikes/>}/>
-            <Route path="/mydislikes" element={<MyDislikes/>}/>
+            <Route path="/likes" element={<MyLikes/>}/>
+            <Route path="/dislikes" element={<MyDislikes/>}/>
         </Routes>
     </div>
   );
